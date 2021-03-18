f454b82476931eb27bfcd704918fdd1cd56947d5
swati.kalra <swati.kalra@localhost>
2021-03-18 14:22:02 +0530

    Updated tests

--- Object Repository/GetAPI.rs
+++ Object Repository/GetAPI.rs
@@ -41,6 +41,9 @@ ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
 
 WS.verifyResponseStatusCode(response, 200)
 
-assertThat(response.getStatusCode()).isEqualTo(200)</verificationScript>
+assertThat(response.getStatusCode()).isEqualTo(200)
+
+
+GlobalVariable.variable</verificationScript>
    <wsdlAddress></wsdlAddress>
 </WebServiceRequestEntity>
